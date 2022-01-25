1851837d7d089bde3db33919fe14cd3c49a5f7cb
sebygalla <sebygalla@hotmail.com>
2022-01-25 12:59:35 -0300

    menu hamburguesa!

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+/*icono menu*/
+const navMenu = document.querySelector('.navbar__menu');
+console.log(navMenu);
+/*enlaces*/
+const navItem = document.querySelector('.navbar__item');
+
+/* evento click en menu hamburguesa*/
+navMenu.addEventListener('click', function(){
+    navItem.classList.toggle('navbar__item--on');
+})
