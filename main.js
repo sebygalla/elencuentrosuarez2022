e94a5347fdfc64860797e0839165cb699c78820f
sebygalla <sebygalla@hotmail.com>
2022-01-26 10:32:48 -0300

    refactor
    ir arriba/ btn whatsapp

--- main.js
+++ main.js
@@ -8,3 +8,23 @@ const navItem = document.querySelector('.navbar__item');
 btnMenu.addEventListener('click', function(){
     navItem.classList.toggle('navbar__item--on');
 })
+
+//Scroll Efect
+
+const btn_scrolltop = document.getElementById('btn_scrolltop')
+btn_scrolltop.addEventListener('click', function () {
+  window.scrollTo(0, 0)
+})
+
+window.onscroll = function () {
+  add_btn_scrolltop()
+};
+
+
+const add_btn_scrolltop = function () {
+  if (window.scrollY < 300) {
+    btn_scrolltop.classList.remove('btn__scrolltop--on')
+  } else {
+    btn_scrolltop.classList.add('btn__scrolltop--on')
+  }
+};
