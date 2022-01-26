79cb0399f955ced2381ce75c6ee86dd77f126e0c
sebygalla <sebygalla@hotmail.com>
2022-01-26 00:21:23 -0300

    refactorizacion
    rehma container arreglo

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 /*icono menu*/
-const navMenu = document.querySelector('.navbar__menu');
-console.log(navMenu);
+const btnMenu = document.querySelector('.navbar__menu');
+
 /*enlaces*/
 const navItem = document.querySelector('.navbar__item');
 
 /* evento click en menu hamburguesa*/
-navMenu.addEventListener('click', function(){
+btnMenu.addEventListener('click', function(){
     navItem.classList.toggle('navbar__item--on');
 })
