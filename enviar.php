1a796621152c0e5956cd7441e650d7c597c32ed2
sebygalla <sebygalla@hotmail.com>
2022-01-31 20:45:35 -0300

    formulario php
    form html

---
+++ enviar.php
@@ -0,0 +1,10 @@
+<?php
+    $destino= "sebygalla@hotmail.com";
+    $nombre= $_POST["nombre"];
+    $correo= $_POST["correo"];
+    $telefono= $_POST["telefono"];
+    $mensaje= $_POST["mensaje"];
+    $contenido= "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTelefono: " . $telefono . "\nMensaje: " . $mensaje;
+    mail($destino, "Contacto", $contenido);
+    header("Location:gracias.html");
+?>
